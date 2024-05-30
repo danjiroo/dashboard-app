f48973ac7fb860525c0b265d5debc4a57fe699a5
Dan Quesada <danilo.q@pandolink.com>
2024-05-31 00:00:27 +0800

    installed dotenv

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,21 @@
 /* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      // this generate: '/api/your-endpoint' => '/your-endpoint'
-      '/api': {
-        target: process.env.VITE_API_BASE_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+dotenv.config(); 
+
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      }
     }
   }
-})
-
-
-
-
-
+} )
