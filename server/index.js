cba5814383e6e594a2994ad157c380b435852f1c
Dan Quesada <danilo.q@pandolink.com>
2024-05-31 22:55:10 +0800

    cors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-undef */
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.json());
 
 mongoose
