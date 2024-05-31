d3e0617549653ff30f5d163e10d4a00882ae2d9e
Dan Quesada <danilo.q@pandolink.com>
2024-06-01 00:18:45 +0800

    vercel.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
         '/api': {
           target: 'http://localhost:5000',
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
+          rewrite: (path) => path.replace(/^\/api/, '/api'),
         }
       } : undefined,
     },
